6d11a1d754b01967ef987e6db8f4b011d981f664
Luc Sorel-Giffo <luc.sorel@gmail.com>
2024-02-25 12:39:31 +0100

    refactor(tests.integrations): distinguish sequence and component tests for ecoindex

--- tests/integrations/ecoindex/test_ecoindex.py
+++ tests/integrations/ecoindex/test_ecoindex.py
@@ -5,7 +5,7 @@ from tests.modules.ecoindex import ecoindex
 
 def test_trace_to_sequence_puml():
     decorated_ecoindex = trace_to_sequence_puml(
-        export_file_path_tpl='${function_module}/${function_name}-${datetime_millis}.puml'
+        export_file_path_tpl='${function_module}/${function_name}-${datetime_millis}-seq.puml'
     )(ecoindex)
     ecoindex_score = decorated_ecoindex(dom_elements_nb=960, requests_nb=70, size_kb=1500)
 
@@ -14,7 +14,7 @@ def test_trace_to_sequence_puml():
 
 def test_trace_to_component_puml():
     decorated_ecoindex = trace_to_component_puml(
-        export_file_path_tpl='${function_module}/${function_name}-${datetime_millis}.puml'
+        export_file_path_tpl='${function_module}/${function_name}-${datetime_millis}-comp.puml'
     )(ecoindex)
     ecoindex_score = decorated_ecoindex(dom_elements_nb=960, requests_nb=70, size_kb=1500)
 
